61c5ca7f9b6432398e76370b00a2b95a4bad83b7
Seth <colts18seth@gmail.com>
2020-04-03 22:05:02 -0400

    started project

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from boggle import Boggle
+from flask import Flask, render_template
 
+app = Flask(__name__)
 boggle_game = Boggle()
+
+@app.route('/')
+def index():
+    """return homepage"""
+
+    return render_template("index.html", boggle_game=boggle_game) 
